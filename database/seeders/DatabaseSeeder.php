4aac7e90de52197997d2ad38efbe59f139d23034
Teacher <sapdevm@gmail.com>
2024-05-08 19:04:45 +0500

    2.1.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -29,5 +30,7 @@ class DatabaseSeeder extends Seeder
             AttributeValueSeeder::class,
             ProductVariantSeeder::class,
         ]);
+
+        Artisan::call('app:update-home-products');
     }
 }
--- resources/views/client/app/alert.blade.php
+++ resources/views/client/app/alert.blade.php
@@ -1,19 +1,38 @@
-@if($errors->any())
+@if(session('success'))
     <div class="position-absolute end-0 m-3">
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <ul class="mb-0">
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <i class="bi-check-circle-fill"></i> {!! session('success') !!}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+@elseif(isset($success))
+    <div class="position-absolute end-0 m-3">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <i class="bi-check-circle-fill"></i> {!! $success !!}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>
 @endif
-@if(isset($success))
+@if(session('error'))
     <div class="position-absolute end-0 m-3">
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ $success }}
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <i class="bi-x-circle-fill"></i> {!! session('error') !!}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+@elseif(isset($error))
+    <div class="position-absolute end-0 m-3">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <i class="bi-x-circle-fill"></i> {!! $error !!}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    </div>
+@elseif($errors->any())
+    <div class="position-absolute end-0 m-3">
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            @foreach($errors->all() as $error)
+                <div><i class="bi-x-circle-fill"></i>  {{ $error }}</div>
+            @endforeach
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>
